adb087470ae61c39822e431a9b5f1ceba7c6fcd6
Tommy H Yeargin Jr <yeargin2021@gmail.com>
2024-06-25 10:50:17 -0400

    New commit.

--- Hello.c
+++ Hello.c
@@ -1,6 +1,23 @@
 //
 // Created by Tommy H Yeargin Jr on 6/25/24.
 //
+/*
+    cLion - First C Project Using cLion IDE
+    Copyright (C) 2024 Tommy H Yeargin Jr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
